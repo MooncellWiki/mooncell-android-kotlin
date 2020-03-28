557d731c0aded88673ca1544405bdae159e2a4c4
StarHeartHunt <starheart233@gmail.com>
2020-03-28 21:47:45 +0800

    tabLayout webViewChromeClient & style optimization

--- app/src/main/java/wiki/fgo/app/PageFragment.kt
+++ app/src/main/java/wiki/fgo/app/PageFragment.kt
@@ -1,11 +1,19 @@
 package wiki.fgo.app
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.os.Message
 import android.view.*
+import android.webkit.CookieManager
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.nav_header.*
+import kotlinx.android.synthetic.main.webview.*
 import wiki.fgo.app.McWebview.WebviewInit
 
 
@@ -18,14 +26,28 @@ class PageFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_page, container, false)
         val page = arguments?.getInt(PAGE_NUM)
         val cacheDir = arguments?.getString("cacheDirPath")
-        setHasOptionsMenu(true)
         val tvHello: WebView = view.findViewById(R.id.webView_replica)
 
         if (cacheDir != null) {
             WebviewInit.setWebView(tvHello, cacheDir)
         }
         tvHello.webViewClient = object : WebViewClient() {}
-        tvHello.loadUrl("https://fgo.wiki")
+        tvHello.webChromeClient = object : WebChromeClient() {
+            override fun onCreateWindow(
+                view: WebView,
+                dialog: Boolean,
+                userGesture: Boolean,
+                resultMsg: Message?
+            ): Boolean {
+                val result: WebView.HitTestResult = view.hitTestResult
+                val data: String? = result.extra
+                val context = view.context
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(data))
+                context.startActivity(browserIntent)
+                return false
+            }
+        }
+        tvHello.loadUrl("https://fgo.wiki/index.php?title=首页&mobileaction=toggle_view_mobile")
         return view
     }
 
