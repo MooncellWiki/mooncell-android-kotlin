48decb2b2c23f67affb4d174eeedd66c58d7eccd
StarHeartHunt <starheart233@gmail.com>
2020-03-31 07:47:44 +0800

    optimize import

--- app/src/main/java/wiki/fgo/app/adapter/TabAdapter.kt
+++ app/src/main/java/wiki/fgo/app/adapter/TabAdapter.kt
@@ -2,9 +2,7 @@ package wiki.fgo.app.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.adapter.FragmentViewHolder
 import wiki.fgo.app.fragment.SwipeRefreshWebViewFragment
 
 class TabAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
