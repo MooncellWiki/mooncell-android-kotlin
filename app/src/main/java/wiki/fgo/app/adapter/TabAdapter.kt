3c98ef4495bec2ed8709278b127843aef3868569
夕舞八弦 <1677759063@qq.com>
2020-03-30 21:22:12 +0800

    cache fragment

--- app/src/main/java/wiki/fgo/app/adapter/TabAdapter.kt
+++ app/src/main/java/wiki/fgo/app/adapter/TabAdapter.kt
@@ -2,7 +2,9 @@ package wiki.fgo.app.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentViewHolder
 import wiki.fgo.app.fragment.SwipeRefreshWebViewFragment
 
 class TabAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
@@ -13,5 +15,4 @@ class TabAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
         fragments[position] = f
         return f
     }
-
 }
